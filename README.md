867ae65577014341282a461534576aecd7f463f9
a-stanisz <hello@adamstaniszewski.com>
2021-03-23 14:48:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# furry-engine
+This is a training project for a recap of Node.js fundamentals coding along Maximilian Schwarzmüller Node.js course - Section 3. 
---
+++ app.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+      res.write('<html>');
+      res.write('<head><title>Enter a message</title></head>');
+      res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+      res.write('</html>');
+      return res.end();
+
+  }
+//   process.exit();
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  };
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>A HTML Page generated from Node.js</title></head>');
+  res.write('<body><h1>Hello from a Node.js server!</h1></body>');
+  res.write('</html>');
+  res.end();
+});
+
+server.listen(3000);
