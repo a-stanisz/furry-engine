ca73b4dc7c539a53ca9861eff8a6318fd74cce84
a-stanisz <hello@adamstaniszewski.com>
2021-03-23 17:03:32 +0100

    Refactor to using module exports

--- app.js
+++ app.js
@@ -1,41 +1,7 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-      res.write('<html>');
-      res.write('<head><title>Enter a message</title></head>');
-      res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-      res.write('</html>');
-      return res.end();
+const routes = require('./routes');
 
-  }
-//   process.exit();
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody);
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, () => {
-          res.statusCode = 302;
-          res.setHeader('Location', '/');
-          return res.end();
-      });
-    })
-  };
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>A HTML Page generated from Node.js</title></head>');
-  res.write('<body><h1>Hello from a Node.js server!</h1></body>');
-  res.write('</html>');
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
