a8e665427f35299090532d99c7f26969ae57f4d6
a-stanisz <hello@adamstaniszewski.com>
2021-04-02 15:04:30 +0200

    Add new branch using EJS, install and set EJS

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
