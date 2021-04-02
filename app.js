783143a709c4875b3dc6e3a7c7047a5e50f08386
a-stanisz <hello@adamstaniszewski.com>
2021-04-02 12:52:13 +0200

    Install and setup express-handlebars@3.0

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const path = require('path');
 
 const express = require('express');
 // const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
+
+app.engine('handlebars', expressHbs());
+app.set('view engine', 'handlebars');
+app.set('views', 'views');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
