e0f6b3a7beac52d077b97ac10f479e529b982530
a-stanisz <hello@adamstaniszewski.com>
2021-04-13 16:17:04 +0200

    Deleting card items in UI

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,14 +13,14 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  console.log(Product.findById(prodId, product => {
+  Product.findById(prodId, product => {
     res.render('shop/product-detail', {
       product: product,
       pageTitle: product.title,
       path: '/products'
       // path: `/products/:${product.id}`
     });
-  }));
+  });
 };
 
 exports.getIndex = (req, res, next) => {
@@ -34,9 +34,21 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(prod => prod.id === product.id);
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        };
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      });
+    });
   });
 };
 
@@ -47,6 +59,14 @@ exports.postCart = (req, res, next) => {
   });
   res.redirect('/cart');
 }
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  })
+};
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
