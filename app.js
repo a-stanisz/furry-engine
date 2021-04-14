72e5972c23abc3caeec64c083da623af8e03cd72
a-stanisz <hello@adamstaniszewski.com>
2021-04-14 15:00:11 +0200

    Configure DB connection

--- app.js
+++ app.js
@@ -6,13 +6,23 @@ const app = express();
 
 const errorController = require('./controllers/error');
 
+const db = require('./util/database');
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(express.urlencoded({extended: false}));
+db.execute('SELECT * FROM products')
+  .then((result) => {
+    console.log(result[0], result[1]);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
