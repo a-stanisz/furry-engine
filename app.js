af3f123a04d0be17aad851855eb83f2b4904c99d
a-stanisz <hello@adamstaniszewski.com>
2021-04-15 17:03:57 +0200

    Exchange custom Product-model to sequelize, exchange methods; add to cart, cart, user, user order don't work yet

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 const errorController = require('./controllers/error');
 
-// const db = require('./util/database');
+const sequelize = require('./util/database');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -22,4 +22,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then(result => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
