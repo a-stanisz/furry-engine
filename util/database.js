adf397a84c94dfdce18a7663f5404959bb699cee
a-stanisz <hello@adamstaniszewski.com>
2021-07-27 15:27:48 +0200

    Do some cleanup and general recap after return to the project

--- util/database.js
+++ util/database.js
@@ -1,29 +1,31 @@
 const mongodb = require('mongodb');
-const { get } = require('../routes/admin');
+// const { get } = require('../routes/admin');
 const MongoClient = mongodb.MongoClient;
 
 let _db;
 
 const mongoConnect = (success) => {
-  MongoClient
-    .connect(process.env.DB_URL)
-    .then(client => {
+  MongoClient.connect(process.env.DB_CONN_STR, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then((client) => {
       console.log('Connected!');
       _db = client.db();
       success();
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       throw err;
     });
-}
+};
 
 const getDb = () => {
   if (_db) {
     return _db;
   }
   throw 'No database found!';
-}
+};
 
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
