3f6550b3a2145fecd22d6f0dc389eacf9a1f0bc4
a-stanisz <hello@adamstaniszewski.com>
2021-07-30 12:20:56 +0200

    Add orders functionality

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ class User {
     this.name = name;
     this.email = email;
     this.cart = cart;
-    this._id = id ? new mongodb.ObjectId(id) : null;
+    this._id = id ? ObjectId(id) : null;
   }
 
   save() {
@@ -61,6 +61,75 @@ class User {
       );
   }
 
+  getCart() {
+    const db = getDb();
+    const productsIds = this.cart.items.map((item) => {
+      return item.productId;
+    });
+    return db
+      .collection('products')
+      .find({ _id: { $in: productsIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((product) => {
+          return {
+            ...product,
+            quantity: this.cart.items.find((i) => {
+              return i.productId.toString() === product._id.toString();
+            }).quantity,
+          };
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter((item) => {
+      return item.productId.toString() !== productId.toString();
+    });
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: { items: updatedCartItems } } }
+      );
+  }
+
+  addOrder() {
+    const db = getDb();
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            name: this.name,
+            email: this.email,
+          },
+        };
+        return db.collection('orders').insertOne(order);
+      })
+      .then((result) => {
+        this.cart = { items: [] };
+        return db
+          .collection('users')
+          .updateOne(
+            { _id: new ObjectId(this._id) },
+            { $set: { cart: { items: [] } } }
+          );
+      })
+      .catch((err) => console.log(err));
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection('orders')
+      .find({ 'user._id': new ObjectId(this._id) })
+      .toArray();
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
@@ -76,19 +145,5 @@ class User {
       });
   }
 }
-
-// const Sequelize = require('sequelize');
-// const sequelize = require('../util/database');
-
-// const User = sequelize.define('user', {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     allowNull: false,
-//     primaryKey: true
-//   },
-//   name: Sequelize.STRING,
-//   email: Sequelize.STRING
-// });
 
 module.exports = User;
