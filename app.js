ba7505739d7cf47330cef26e0b5204f502c083a5
a-stanisz <hello@adamstaniszewski.com>
2021-09-03 16:55:57 +0200

    Add config for using session

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const path = require('path');
 
 const express = require('express');
-
-const app = express();
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
-
 const User = require('./models/user');
+
+const app = express();
+const store = new MongoDBStore({
+  uri: process.env.DB_CONN_STR,
+  collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,6 +23,14 @@ const authRoutes = require('./routes/auth');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'longSecretString',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 
 app.use((req, res, next) => {
   User.findById('6130b8bf2a22e038600a3ddf')
