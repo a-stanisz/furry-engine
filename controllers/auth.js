cddc1e4a9a697fc076d9a22d663ea34f63590601
a-stanisz <hello@adamstaniszewski.com>
2021-09-03 15:15:13 +0200

    Fix minor bug and typos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,6 @@
 exports.getLogin = (req, res, next) => {
-  Order.find({ 'user.userId': req.user._id })
-    .then((orders) => {
-      res.render('auth/login', {
-        path: '/login',
-        pageTitle: 'Log in',
-      });
-    })
-    .catch((err) => console.log(err));
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Log in',
+  });
 };
