7175ba013ae41d78c62c9cb84189807d8a4aeb09
a-stanisz <hello@adamstaniszewski.com>
2021-09-07 13:36:32 +0200

    Add CSRF protection

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Log in',
-    isAuthenticated: false
+    // isAuthenticated: false
   });
 };
 
@@ -14,7 +14,7 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Sign up',
-    isAuthenticated: false
+    // isAuthenticated: false
   });
 };
 
