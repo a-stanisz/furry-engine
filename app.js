d827b58d97b1bab2f2ec8d739cf17ad66373463a
a-stanisz <hello@adamstaniszewski.com>
2021-09-11 12:41:13 +0200

    Add 500 generic error page and error handling

--- app.js
+++ app.js
@@ -43,10 +43,15 @@ app.use((req, res, next) => {
   }
   User.findById(req.session.user._id)
     .then((user) => {
+      if (!user) {
+        return next();
+      }
       req.user = user;
       next();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      throw new Error(err);
+    });
 });
 
 app.use((req, res, next) => {
@@ -59,7 +64,13 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500);
+
 app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  res.redirect('/500');
+})
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
