01a8749a5555d6d779473b5ccd84f1d6679555de
a-stanisz <hello@adamstaniszewski.com>
2021-09-12 16:27:01 +0200

    Populate invoice pdf with order data

--- controllers/shop.js
+++ controllers/shop.js
@@ -138,13 +138,26 @@ exports.getInvoice = (req, res, next) => {
     const invoiceName = `invoice-${orderId}.pdf`;
     const invoicePath = path.join('data', 'invoices', invoiceName);
     
-    const pdfDoc = new PdfDocument();
     res.setHeader('Content-Type', 'application/pdf');
     res.setHeader('Content-Disposition', `inline; filename=${invoiceName}`);
+    const pdfDoc = new PdfDocument();
     pdfDoc.pipe(fs.createWriteStream(invoicePath));
     pdfDoc.pipe(res);
-
-    pdfDoc.text('Hello World!');
+    pdfDoc.fontSize(26).text('Invoice', { underline: true });
+    pdfDoc.text('--------------------------------');
+    let totalPrice = 0;
+    order.products.forEach(prod => {
+      totalPrice += product.prod.quantity * product.prod.price;
+      pdfDoc
+      .fontSize(14)
+      .text(
+        `${prod.product.title}: ${prod.product.quantity} x ${prod.product.price}`
+        );
+      });
+    pdfDoc.text('--------------------------------');
+    pdfDoc
+      .fontSize(20)
+      .text(`Total Price: $${totalPrice}`);
     pdfDoc.end();
 
     // fs.readFile(invoicePath, (err, data) => {
