533ba1af96e16de6ff8465662c32b82c8eeb9c1b
a-stanisz <hello@adamstaniszewski.com>
2021-09-12 13:08:56 +0200

    Fix deprecated requires of express-validator

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,4 @@
-const { validationResult } = require('express-validator/check');
+const { validationResult } = require('express-validator');
 
 const ObjectId = require('mongoose').Types.ObjectId;
 
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 
-const { validationResult } = require('express-validator/check');
+const { validationResult } = require('express-validator');
 
 const sendgridMail = require('@sendgrid/mail');
 sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
