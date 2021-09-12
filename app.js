0287167e2e27aff7eded9fe761aed514a02fad6f
a-stanisz <hello@adamstaniszewski.com>
2021-09-12 13:08:27 +0200

    Bring back useUnifiedTopology mongoose setting

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.use((error, req, res, next) => {
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-// mongoose.set('useUnifiedTopology', true);
+mongoose.set('useUnifiedTopology', true);
 
 mongoose
   .connect(process.env.DB_CONN_STR)
